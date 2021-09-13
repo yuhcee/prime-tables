cf54c6c8c299bf08b0c9b56925b63b9787a589bb
yuhcee <egbouchenna1@gmail.com>
2021-09-13 02:40:26 +0100

    add tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,30 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders App without crashing', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const h1Element = screen.getByText(/Prime Numbers Multiplication Table/i);
+  expect(h1Element).toBeInTheDocument();
+});
+
+test('renders input field', () => {
+  render(<App />);
+  const textElement = screen.getByPlaceholderText(
+    /Enter/i
+  );
+  expect(textElement).toBeInTheDocument();
+});
+
+test('renders input field with placeholder text', () => {
+  render(<App />);
+  const placeHolderElement = screen.getByPlaceholderText(
+    /Enter a number limit to generate.../i
+  );
+  expect(placeHolderElement).toBeInTheDocument();
+});
+
+test('renders footer link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Yuhcee/i);
   expect(linkElement).toBeInTheDocument();
 });
