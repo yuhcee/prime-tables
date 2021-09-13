48a6f5999f5413671b7e5cf2cba90a8cca647689
yuhcee <egbouchenna1@gmail.com>
2021-09-13 02:25:27 +0100

    add Footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+export const Footer: React.FC = () => {
+  return (
+    <div>
+      <footer>
+        <div className="footer">
+          &copy;2021 Built with ❤ by{' '}
+          <i>
+            <a href="https://www.github.com/yuhcee">Yuhcee</a>
+          </i>
+        </div>
+      </footer>
+    </div>
+  );
+};
