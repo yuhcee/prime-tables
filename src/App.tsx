373c8a2d3d45b4b504488728f86f84cbd39e6c96
yuhcee <egbouchenna1@gmail.com>
2021-09-13 02:26:07 +0100

    wrap app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { AddNumberLimit } from './components/AddNumberLimit';
+import Table from './components/Table/Index';
+import { Footer } from './components/Footer';
+import { generatePrimeNumbers } from './utils/generatePrimeNumbers';
 
 function App() {
+  const [numberInput, setNumberInput] = useState<number>(30);
+  const primeNumbers = generatePrimeNumbers(numberInput);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNumberInput(+e.target.value);
+  };
+
+  useEffect(() => {
+    if (numberInput && numberInput > 150) {
+      alert('This computation might take a while because number is too large!');
+    }
+    if (numberInput && numberInput < 1) {
+      alert('Please enter a valid input!');
+    }
+  }, [numberInput]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Prime Numbers Multiplication Table</h1>
+      <AddNumberLimit onChange={handleChange} />
+      <Table primeNumbers={primeNumbers} />
+      <Footer />
     </div>
   );
 }
